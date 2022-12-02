9e68646847be28bd3227ec10e8a72bb8a94f6634
Pavel Ovchinnikov <opavel@internet.ru>
2022-12-02 12:13:46 +0000

    task: выполнение доп.задания

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ class Figure:
     def volume(self):
         return self.a * self.b * self.c
 
+    def __add__(self, other):
+        if isinstance(other, FigureWithCavity):
+            return FigureWithCavity(self.a + other.a, self.b + other.b, self.c + other.c, other.d)
+        if isinstance(other, FigureList) and (self.a, self.b, self.c) == (other.a, other.b, other.c):
+            return FigureList(self.a, self.b, self.c, other.count + 1)
+        if isinstance(other, Figure):
+            return Figure(self.a + other.a, self.b + other.b, self.c + other.c)
+        return self.volume() + other.volume()
+
 
 class FigureWithCavity(Figure):
     def __init__(self, a, b, c, d):
@@ -14,6 +23,15 @@ class FigureWithCavity(Figure):
     def volume(self):
         return super().volume() - (self.a - self.d) * (self.b - self.d) * (self.c - self.d)
 
+    def __add__(self, other):
+        if isinstance(other, Figure):
+            return FigureWithCavity(self.a + other.a, self.b + other.b, self.c + other.c, self.d)
+        if isinstance(other, FigureWithCavity):
+            return FigureWithCavity(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
+        if isinstance(other, FigureList) and (self.a, self.b, self.c) == (other.a, other.b, other.c):
+            raise ValueError("Нельзя прибавлять фигуру с пустотой к массиву обычных фигур")
+        return self.volume() + other.volume()
+
 
 class FigureList(Figure):
     def __init__(self, a, b, c, count):
@@ -22,3 +40,12 @@ class FigureList(Figure):
 
     def volume(self):
         return super().volume() * self.count
+
+    def __add__(self, other):
+        if isinstance(other, FigureList) and (self.a, self.b, self.c) == (other.a, other.b, other.c):
+            return FigureList(self.a, self.b, self.c, self.count + other.count)
+        if isinstance(other, Figure) and (self.a, self.b, self.c) == (other.a, other.b, other.c):
+            return FigureList(self.a, self.b, self.c, self.count + 1)
+        if isinstance(other, FigureWithCavity):
+            raise ValueError('Нельзя прибавлять к массиву обычных фигур фигуры с пустотой')
+        return self.volume() + other.volume()
